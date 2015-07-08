c40b1c5876be831dedaddaffffa3b35859dfeb46
limekin <kevin.t.jayan@gmail.com>
2015-07-08 20:08:55 +0530

    Implement Listing listing.

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -4,7 +4,23 @@
   <h2>Listings</h2>
   @if ( count($listings) == 0 )
     <p>There are not any listings in the database.</p>
+  @else
+
+    <div class='listings'>
+      @foreach($listings as $listing) 
+	<div class='listing'>
+	  <p>{{ $listing->name }}</p>
+	  <ul>
+	    @foreach($listing->listing_items as $listing_item)
+	      <li>{{ $listing_item->name }}</li>
+	    @endforeach
+	  </ul>
+	</div>
+      @endforeach
+    </div>
+
   @endif
 
   <a href='<?php echo route('listing.create') ?>'>New</a>
-@endsection
+  @endsection
+
